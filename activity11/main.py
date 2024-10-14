34c5682168d94ebfb6efb23712e373c4e6c36b0f
Rossana Lícya <168486966+licyafreoli@users.noreply.github.com>
2024-10-14 18:48:34 -0300

    Update main.py

---
+++ activity11/main.py
@@ -0,0 +1,7 @@
+nome = input("digite qual nome de usuario:")
+senha = float(input("digite sua senha:"))
+
+if nome == "adm" and senha == 123:
+  print("seja bem vindo")
+else: 
+  print("usuario ou senha incorretos")
