9517df94408795189a70590046edaf995af1b03f
Rossana Lícya <168486966+licyafreoli@users.noreply.github.com>
2024-10-14 18:28:20 -0300

    Update main.py

---
+++ activity09/main.py
@@ -0,0 +1,11 @@
+idade = float(input("digite sua idade:"))
+if idade <=12:
+  print("você é criança!")
+elif idade <=17:
+  print("você é adolescente!")
+elif idade <=59:
+  print("você é adulto!")
+elif idade <=10000000:
+  print("você é idoso")
+else:
+  print("você não tem uma idade real")
