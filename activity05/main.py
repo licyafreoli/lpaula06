f71b7a3ed5bdec0e9eaa37cd51caae2c114b6b35
Rossana Lícya <168486966+licyafreoli@users.noreply.github.com>
2024-10-14 16:39:51 -0300

    Update main.py

---
+++ activity05/main.py
@@ -0,0 +1,11 @@
+nota1= float(input("digite qual a nota do aluno 1:"))
+nota2= float(input("digite qual a nota do aluno 2:"))
+nota3= float(input("digite qual a nota do aluno 3:"))
+nota4= float(input("digite qual a nota do aluno 4:"))
+
+media = (nota1+ nota2+nota3+nota4)/4
+
+if media >=6:
+  print ("aprovado")
+else:
+  print("reprovado")
